edd9bfa8cdce2e35d9226cb18dffe47e0b034594
wb-p <wzbury@wp.pl>
2025-05-05 09:33:27 +0300

    Cosmetic change:
    renamed "fragment_notifications.xml" to "fragment_networking.xml" and associated variables accordingly

--- app/src/main/java/catmonit/app/ui/networking/NetworkingFragment.java
+++ app/src/main/java/catmonit/app/ui/networking/NetworkingFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import catmonit.app.databinding.FragmentNotificationsBinding;
+import catmonit.app.databinding.FragmentNetworkingBinding;
 
 public class NetworkingFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentNetworkingBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NetworkingViewModel notificationsViewModel =
+        NetworkingViewModel networkingViewModel =
                 new ViewModelProvider(this).get(NetworkingViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentNetworkingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.bandwidthUsedText;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        networkingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
